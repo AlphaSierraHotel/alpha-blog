0542e13657413dd8f0347510f2637adc096986d6
Allen Hubble <allen@hubtech.ca>
2018-12-05 12:23:31 -0500

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,4 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -21,15 +20,11 @@ class CategoriesController < ApplicationController
   end
 
   def show
-
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
   def category_params
     params.require(:category).permit(:name)
   end
